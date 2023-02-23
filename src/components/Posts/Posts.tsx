a96e1d6800c8bd8a2843cdb7bc87c2a281328ced
setouchi-h <setouchi.kazuki@gmail.com>
2023-02-23 14:58:10 +0900

    update post item

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,13 +1,71 @@
-import { Community } from '@/src/atoms/communitiesAtom';
-import React from 'react';
+import { Community } from "@/src/atoms/communitiesAtom"
+import { Post } from "@/src/atoms/postsAtom"
+import { auth, firestore } from "@/src/firebase/clientApp"
+import usePosts from "@/src/hooks/usePosts"
+import { Stack } from "@chakra-ui/react"
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore"
+import React, { useEffect, useState } from "react"
+import { useAuthState } from "react-firebase-hooks/auth"
+import PostItem from "./PostItem"
+import PostLoader from "./PostLoader"
 
 type PostsProps = {
-    communityData: Community
-    userId?: string
-};
-
-const Posts:React.FC<PostsProps> = () => {
-    
-    return <div>Have a good coding</div>
+  communityData: Community
 }
-export default Posts;
+
+const Posts: React.FC<PostsProps> = ({ communityData }) => {
+  const [user] = useAuthState(auth)
+  const [loading, setLoading] = useState(false)
+  const { postStateValue, setPostStateValue, onVote, onDeletePost, onSelectPost } = usePosts()
+
+  const getPosts = async () => {
+    try {
+      setLoading(true)
+      // get posts for this community
+      const postsQuery = query(
+        collection(firestore, "posts"),
+        where("communityId", "==", communityData.id),
+        orderBy("createdAt", "desc")
+      )
+      const postDocs = await getDocs(postsQuery)
+
+      // store in post state
+      const posts = postDocs.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+      setPostStateValue((prev) => ({
+        ...prev,
+        posts: posts as Post[],
+      }))
+    } catch (error: any) {
+      console.log("getPosts error", error)
+    }
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    getPosts()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <>
+      {loading ? (
+        <PostLoader />
+      ) : (
+        <Stack>
+          {postStateValue.posts.map((item) => (
+            <PostItem
+              key={item.id}
+              post={item}
+              userIsCreator={user?.uid === item.creatorId}
+              userVoteValue={undefined}
+              onVote={onVote}
+              onSelectPost={onSelectPost}
+              onDeletePost={onDeletePost}
+            />
+          ))}
+        </Stack>
+      )}
+    </>
+  )
+}
+export default Posts
