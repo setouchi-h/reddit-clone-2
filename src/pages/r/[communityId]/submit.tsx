66d58f5a78a9b7d1e6af07b911562543bf5b0a83
setouchi-h <setouchi.kazuki@gmail.com>
2023-03-18 00:47:57 +0900

    update submit page

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,14 +1,17 @@
 import { communityState } from "@/src/atoms/communitiesAtom"
+import About from "@/src/components/Community/About"
 import PageContent from "@/src/components/Layout/PageContent"
 import NewPostForm from "@/src/components/Posts/NewPostForm"
 import { auth } from "@/src/firebase/clientApp"
+import useCommunityData from "@/src/hooks/useCommunityData"
 import { Box, Text } from "@chakra-ui/react"
 import { useAuthState } from "react-firebase-hooks/auth"
 import { useRecoilValue } from "recoil"
 
 const SubmitPostPage: React.FC = () => {
   const [user] = useAuthState(auth)
-  const communityStateValue = useRecoilValue(communityState)
+  // const communityStateValue = useRecoilValue(communityState)
+  const { communityStateValue } = useCommunityData()
 
   return (
     <PageContent>
@@ -21,7 +24,11 @@ const SubmitPostPage: React.FC = () => {
       </>
 
       {/* About */}
-      <></>
+      <>
+        {communityStateValue.currentCommunity && (
+          <About communityData={communityStateValue.currentCommunity} />
+        )}
+      </>
     </PageContent>
   )
 }
